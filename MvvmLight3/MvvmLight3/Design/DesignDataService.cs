e475232efc2d5d696e15982cbb186e9f781ce450
touchva <touchva@hotmail.com>
2014-04-26 04:24:00 +0200

    1213

---
+++ MvvmLight3/MvvmLight3/Design/DesignDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MvvmLight3.Model;
+
+namespace MvvmLight3.Design
+{
+    public class DesignDataService : IDataService
+    {
+        public void GetData(Action<DataItem, Exception> callback)
+        {
+            // Use this to create design time data
+
+            var item = new DataItem("Welcome to MVVM Light [design]");
+            callback(item, null);
+        }
+    }
+}
